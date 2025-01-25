16e7aabfd42959453b0ca4c0306ce785a6150f49
MartinC00 <martincabreramathiasen@gmail.com>
2025-01-24 22:22:07 -0300

    config server settings

--- flight_app/hotels_service/src/main/java/microservices1/flight_app/hotels_service/controller/HotelController.java
+++ flight_app/hotels_service/src/main/java/microservices1/flight_app/hotels_service/controller/HotelController.java
@@ -3,7 +3,6 @@ package microservices1.flight_app.hotels_service.controller;
 import lombok.RequiredArgsConstructor;
 import microservices1.flight_app.hotels_service.model.Hotel;
 import microservices1.flight_app.hotels_service.service.iHotelService;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
